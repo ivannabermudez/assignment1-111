07886a69a3e5fdac9a03dcc1b1a9d4fa3059fcd0
ivannabermudez <ivannabermudez@outlook.com>
2023-04-24 08:59:10 -0700

    assignment 1

--- app/__init__.py
+++ app/__init__.py
@@ -15,3 +15,13 @@ def products():
         "<li>%s</li>" % product for product in product_list
     )
     return "<ul>%s</ul>" % bullet_list
+
+@app.get("/")                          # we can now use our onjects method "get" as a decorator 
+def index():                           # a decorator wraps a function 
+    me = {                               # we create a new dictionary with key/value pairs.
+        "first_name": "Ivanna",
+        "last_name": "Bermudez",
+        "hobbies": "Linguistics",
+    
+    }
+    return me 
